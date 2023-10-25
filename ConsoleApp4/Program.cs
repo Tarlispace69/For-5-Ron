ca55cba408d1209d54e9c12b839f49a7acb517f8
Tarlispace69 <116681079+Tarlispace69@users.noreply.github.com>
2023-10-25 13:11:41 +0300

    For-5-Ron Complete

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = 0, signMistakes = 0, otherMistakes = 0, i = 0, passed = 0;
+
+            Console.Write("Enter the number of test takers: ");
+            tests = int.Parse(Console.ReadLine());
+
+            //checking each tester
+            for(i=0; i < tests; i++)
+            {
+                // receiving number and type of mistakes for each tester
+                // +1 for simpler understanding
+                Console.Write($"Test taker {i+1}\nEnter number of failed sign questions: ");
+                signMistakes = int.Parse(Console.ReadLine());
+                Console.Write("Enter other mistakes: ");
+                otherMistakes = int.Parse(Console.ReadLine());
+
+                // checking if it's a pass or fail
+                if (otherMistakes > 3 || signMistakes != 0)
+                {
+                    Console.WriteLine("Fail");
+                }
+                else
+                {
+                    Console.WriteLine("Pass");
+                    passed++;
+                }
+            }
+            Console.WriteLine($"Passed tests: {passed}\nPercentage of passed tests: {(passed / (float)tests) * 100}%");
+            Console.ReadLine();
+
+        }
+    }
+}
